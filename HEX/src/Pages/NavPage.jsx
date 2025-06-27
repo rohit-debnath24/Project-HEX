aa1f804d40f59768300e7d64696afe38be51c9ce
rohit-debnath24 <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-06-28 04:05:09 +0530

    navpage made responsive

--- HEX/src/Pages/NavPage.jsx
+++ HEX/src/Pages/NavPage.jsx
@@ -3,36 +3,36 @@ function NavPage() {
         <>
 
             <div className=" flex flex-cols-3 gap-0 mt-10 ">
-                <div className="w-7 h-180  bg-[#231b17]"></div>
+                <div className="md:w-7 w-1  h-180  bg-[#231b17]"></div>
                 <div className=" w-screen border-2 bg-[#100A06]">
                     <div className="  flex justify-center">
 
 
-                        <div className="w-180 flex justify-center text-center text-7xl text-amber-50 pt-10 "> <div>One home for analytics,AI,and apps</div></div>
+                        <div className="w-180 flex justify-center text-center text-3xl md:text-7xl text-amber-50 pt-10 "> <div>One home for analytics,AI,and apps</div></div>
 
 
                     </div>
                     <div className="  flex justify-center">
 
-                        <div className=" mt-4 flex text-center justify-center text-xl text-amber-500 pt-0">
+                        <div className=" mt-4 flex text-center justify-center md:text-xl text-amber-500 break-all pt-0">
                             Hex allows teams to work together across one platform, making it easy to find answers in <br />your data, share them with stakeholders, and make decisions with confidence.
                         </div>
 
                     </div>
 
-                    <div className="flex flex-cols-3 gap-10 justify-center mb-50 mt-20 ">
-                        <div className="p-1 bg-[url('https://th.bing.com/th/id/OIP.xkMw8PFsh_EXy7z1zABhIQHaEK?w=1280&h=720&rs=1&pid=ImgDetMain')] bg-cover  pt-7" >
-                            <div className=" text-sky-50 w-80 pt-20 pl-5 h-60"><img src="https://png.pngtree.com/png-vector/20240129/ourmid/pngtree-business-report-statistic-png-image_11567573.png" class="w-50 h-45 md:absolute md:top-265 md:left-75 "></img>NOTEBOOK <div>Graph Paper Notebooks - JetPens
+                    <div className=" grid md:flex grid-cols-1 md:flex-cols-3 gap-10 mb-50 mt-20 justify-center ">
+                        <div className="p-1 bg-[url('https://th.bing.com/th/id/OIP.xkMw8PFsh_EXy7z1zABhIQHaEK?w=1280&h=720&rs=1&pid=ImgDetMain')] bg-cover  pt-7  w-80   m-auto" >
+                            <div className=" text-sky-50 w-80 md:pt-20 pl-5 h-60"><img src="https://png.pngtree.com/png-vector/20240129/ourmid/pngtree-business-report-statistic-png-image_11567573.png" class="md:w-50 md:h-45 w-25 h-25  md:absolute md:top-265 md: md:block md:ml-10"></img>NOTEBOOK <div class="overflow-hidden break-words">Graph Paper Notebooks - JetPens
                                 Featuring a grid pattern of horizontal and vertical lines, these graph paper notebooks are a natural choice for sketches and illustrations.</div></div>
 
                         </div>
-                        <div className="p-1 bg-[url('https://th.bing.com/th/id/OIP.xkMw8PFsh_EXy7z1zABhIQHaEK?w=1280&h=720&rs=1&pid=ImgDetMain')] bg-cover  pt-7" >
-                            <div className=" text-sky-50 w-80 pt-20 pl-5 h-60"><img src="https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-notebook-with-a-stock-market-graph-for-illustration-png-image_9201120.png" class="w-40 h-45 md:absolute md:top-265 md:left-172 rotate-y-190 rotate-z-[10deg]"></img>NOTEBOOK <div>Graph Paper Notebooks - JetPens
+                        <div className="p-1 bg-[url('https://th.bing.com/th/id/OIP.xkMw8PFsh_EXy7z1zABhIQHaEK?w=1280&h=720&rs=1&pid=ImgDetMain')] bg-cover  md:pt-7  w-80   m-auto " >
+                            <div className=" text-sky-50 w-80 md:pt-20 pl-5 h-60"><img src="https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-notebook-with-a-stock-market-graph-for-illustration-png-image_9201120.png" class="md:w-40 md:h-45 w-25 h-25 md:absolute md:top-260 md: rotate-y-190 rotate-z-[10deg]  md:block md:ml-10"></img>NOTEBOOK <div>Graph Paper Notebooks - JetPens
                                 Featuring a grid pattern of horizontal and vertical lines, these graph paper notebooks are a natural choice for sketches and illustrations.</div></div>
 
                         </div>
-                        <div className="p-1 bg-[url('https://th.bing.com/th/id/OIP.xkMw8PFsh_EXy7z1zABhIQHaEK?w=1280&h=720&rs=1&pid=ImgDetMain')] bg-cover  pt-7" >
-                            <div className=" text-sky-50 w-80 pt-20 pl-5 h-60"><img src="https://www.maintworld.com/var/ezwebin_site/storage/images/media/images/screenshot-apm-integrity-overview-dashboard-exploded-tablet-1792x1280/55201-1-eng-GB/screenshot-apm-integrity-overview-dashboard-exploded-tablet-1792x1280.png" class="w-50 h-30 md:absolute md:top-270 md:left-255 rotate-0"></img>NOTEBOOK <div>Graph Paper Notebooks - JetPens
+                        <div className="p-1 bg-[url('https://th.bing.com/th/id/OIP.xkMw8PFsh_EXy7z1zABhIQHaEK?w=1280&h=720&rs=1&pid=ImgDetMain')] bg-cover m-x-4 pt-7 w-80   m-auto" >
+                            <div className=" text-sky-50  md:w-80 md:pt-20 pl-5 h-60"><img src="https://www.maintworld.com/var/ezwebin_site/storage/images/media/images/screenshot-apm-integrity-overview-dashboard-exploded-tablet-1792x1280/55201-1-eng-GB/screenshot-apm-integrity-overview-dashboard-exploded-tablet-1792x1280.png" class="md:w-50 md:h-30 w-25 h-20 md:absolute md:top-270 md:pl-10 rotate-0 md:block"></img>NOTEBOOK <div>Graph Paper Notebooks - JetPens
                                 Featuring a grid pattern of horizontal and vertical lines, these graph paper notebooks are a natural choice for sketches and illustrations.</div></div>
 
                         </div>
@@ -40,19 +40,19 @@ function NavPage() {
                     </div>
 
                     <div className="flex justify-center">
-                        <div className=" grid grid-cols-2 md:absolute md:top-347 left- md:w-200 h-25 border-b-2 border-amber-50 z-[] ">
+                        <div className=" grid grid-cols-2 absolute md:top-347 top-645 border-dotted w-80  md:w-200 h-15 md:h-25 border-b-2 border-amber-300 z-[] ">
                             
-                            <div className=" border-l-1  border-amber-300 border-r-1"></div>
-                            <div className=" border-l-1  border-amber-300 border-r-1"></div>
+                            <div className=" border-l-1 border-dotted border-amber-300 border-r-1"></div>
+                            <div className=" border-l-1 border-dotted border-amber-300 border-r-1"></div>
                             
                         </div>
                     </div>
                     <div className="flex justify-center">
-                            <div className="absolute top-368 bg-black border-dotted text-orange-300 border-2 border-amber-300">UNDERLYING HEX PLATFORM</div>
+                            <div className="absolute top-657 md:top-368 bg-black border-dotted text-orange-300 border-2 text-[10px] md:text-[20px] border-amber-300">UNDERLYING HEX PLATFORM</div>
                     </div>
 
                 </div>
-                <div className="w-7 h-180 bg-[#231b17]"></div>
+                <div className="md:w-7 w-1 h-180 bg-[#231b17]"></div>
             </div>
         </>
     )
