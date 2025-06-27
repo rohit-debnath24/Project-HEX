9b88b74cd86f040dd560229febcb28776ab407a5
rohit-debnath24 <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-06-28 02:54:20 +0530

    res.....

--- HEX/src/Pages/LandingPage.jsx
+++ HEX/src/Pages/LandingPage.jsx
@@ -8,7 +8,7 @@ function LandingPage() {
                     <div className="md:ml-10 ml-1 text-center  text-transparent bg-clip-text bg-[linear-gradient(55deg,white_20%,rgba(245,192,192,0.8)_100%)]  mt-10 text-5xl md:text-8xl md:w-180 md:h-50 h-35 bg-amber-500 font-medium">
                         Go deeper than<br/> dashboards
                     </div>
-                    <div className="  md:w-150 md:mt-5 text-center text-amber-800 break-words text-2xl">
+                    <div className=" p-x-6 md:w-150 md:mt-5 text-center text-amber-800 break-words text-2xl">
                         Hex is the AI-powered analytics workspace built for teams driving faster answers and better decisions.
                     </div>
                     <div className="flex justify-around m-auto">
