92340d93dc6653004b141252a4b7b83306c401cd
rohit-debnath24 <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-06-28 02:24:28 +0530

    secNav responsiveness fixed

--- HEX/src/components/SecNav.jsx
+++ HEX/src/components/SecNav.jsx
@@ -15,20 +15,20 @@ const SecNav = () => {
     <>
     <div ></div>
       <div className= {`${sticky ? 'fixed top-[66px] left-0 right-0 shadow-md bg-[#100A06] z-50' : 'relative'} 
-                   border-2   py-1 justify-center transition-all duration-500  grid grid-cols-[20px_1fr_20px] gap-0   
+                   border-2   py-1 justify-center hidden  transition-all duration-500  sm:grid grid-cols-[20px_1fr_20px] gap-0   
                   `}>
-        <div className=" min-h-6  sm:bg-[#231b17]"></div>
-        <div className=" min-h-6  border-2    py-1 flex justify-center sm:text-[10px] md:text-[15px] text-center items-center">
-          <div className="sm:grid hidden  sm:grid-cols-12  min-h-6 w-full">
-            <div className=" grid items-center sm:col-span-6  sm:text-[10px] md:text-[15px]  min-h-6 text-left pl-10 lg:text-3xl text-[#dad2ce] font-bold">An integrated workspace for analytics</div>
-            <div className="   sm:col-span-2 grid justify-center sm:text-[10px] md:text-[15px] text-center items-center min-h-6 text-[#bea89d] " ><a href="#"> I Dive-deep analysis </a></div>
-            <div className="   sm:col-span-2 grid justify-center sm:text-[10px] md:text-[15px] text-center items-center min-h-6 text-[#bea89d]  "><a href="#IntDataPage"> I Interactive data apps </a></div>
-            <div className="   sm:col-span-2 grid justify-center sm:text-[10px] md:text-[15px] text-center items-center min-h-6 text-[#bea89d] "><a href="#SelfServePage">I Self-serve exploration </a> </div>
+        <div className=" min-h-6  bg-[#231b17]"></div>
+        <div className=" min-h-6  border-2    py-1  text-[10px] md:text-[15px] text-center items-center">
+          <div className="grid  grid-cols-12  min-h-6 w-full">
+            <div className=" grid items-center col-span-6  text-[10px] md:text-[15px]  min-h-6 text-left pl-10 lg:text-3xl text-[#dad2ce] font-bold">An integrated workspace for analytics</div>
+            <div className="   col-span-2 grid justify-center text-[10px] md:text-[15px] text-center items-center min-h-6 text-[#bea89d] " ><a href="#"> I Dive-deep analysis </a></div>
+            <div className="   col-span-2 grid justify-center text-[10px] md:text-[15px] text-center items-center min-h-6 text-[#bea89d]  "><a href="#IntDataPage"> I Interactive data apps </a></div>
+            <div className="   col-span-2 grid justify-center text-[10px] md:text-[15px] text-center items-center min-h-6 text-[#bea89d] "><a href="#SelfServePage">I Self-serve exploration </a> </div>
 
 
           </div>
         </div>
-        <div className=" min-h-6   sm:bg-[#231b17]"></div>
+        <div className=" min-h-6   bg-[#231b17]"></div>
       </div>
     </>
   )
