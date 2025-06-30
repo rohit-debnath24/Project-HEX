61f835f2b4b6d016415e3dc4e760f4b32b1d9c03
rohit-debnath24 <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-06-30 15:31:02 +0530

    build with any page completed

--- HEX/src/Pages/BuildAnythingPage.jsx
+++ HEX/src/Pages/BuildAnythingPage.jsx
@@ -11,22 +11,103 @@ const BuildAnythingPage = () => {
           <div class=" min-w-300 lg:h-[1640px] text-2xl grid grid-cols-4  grid-rows-24 gap-x-5   ">
 
             <div class=" col-span-1 border-l-2 border-r-2 border-amber-50 row-span-4 row-start-1 border-dashed"></div>
-            <div class=" col-span-1 bg-amber-600 border-2 border-amber-50 row-span-7 row-start-5">1</div>
-            <div class=" row-span-5 bg-amber-100 border-2 border-amber-50 col-span-1 row-start-12">2</div>
+            <div class=" col-span-1 border-2 border-amber-50 row-span-7 row-start-5
+            grid grid-rows-6 grid-cols-2">
+              {/* <div className="col-span-2 row-span-1 border-3 border-amber-700">1</div> */}
+              <div className="col-span-2 row-span-2 border-3  grid grid-cols-1 grid-rows-4"><div className='col-span-1 row-span-1 text-2xl font-medium text-amber-50'>Data Clustering</div><div className='col-span-1 row-span-3 text-[15px] font-medium text-[#F5C0C0]'>Unleash the power of data clustering—an unsupervised machine learning technique that uncovers patterns and groups similar data together without the need for labeled data.</div></div>
+              <div className="col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300"><img src="https://i.pinimg.com/736x/65/87/b0/6587b0abeef98914bd7cd2aac392811c.jpg" className='bg-cover h-full w-full'></img></div>
+              <div className="col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300"><img src="https://i.pinimg.com/736x/65/87/b0/6587b0abeef98914bd7cd2aac392811c.jpg" className='bg-cover h-full w-full'></img></div>
+              <div className="col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300"><img src="https://i.pinimg.com/736x/65/87/b0/6587b0abeef98914bd7cd2aac392811c.jpg" className='bg-cover h-full w-full'></img></div>
+              <div className="col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300"><img src="https://i.pinimg.com/736x/65/87/b0/6587b0abeef98914bd7cd2aac392811c.jpg" className='bg-cover h-full w-full'></img></div>
+           </div>
+            <div class=" row-span-5  border-2 border-amber-50 col-span-1 row-start-12
+             grid grid-cols-3 grid-rows-4">
+              <div className='col-span-3 row-span-2 border-3 border-black grid grid-cols-1 grid-rows-4'><div className='col-span-1 row-span-1 text-2xl font-medium text-amber-50'>Data Clustering</div><div className='col-span-1 row-span-3 text-[15px] font-medium text-[#F5C0C0]'>Unleash the power of data clustering—an unsupervised machine learning technique that uncovers patterns and groups similar data together without the need for labeled data.</div></div>
+             
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://cdn.vectorstock.com/i/500p/43/83/rainbow-dots-vector-94383.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://cdn5.vectorstock.com/i/1000x1000/38/39/geometric-abstract-pattern-vector-21683839.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://media.istockphoto.com/id/2029197402/vector/colorful-half-tone-background.webp?b=1&s=612x612&w=0&k=20&c=rJUunnAplTC2gdzjb4cIqiU9xMXfEBxcmSNAH_y3alY=" className='bg-cover w-full h-full' ></img></div>
+            </div>
+
             <div class=" col-span-1  border-r-2 border-l-2 border-amber-50 row-span-8 border-b-2 row-start-17 border-dashed"></div>
 
             <div class=" col-span-1  border-l-2 border-amber-50 row-span-1 row-start-6 border-dashed"></div>
-            <div class=" col-span-1 bg-amber-700 border-2 border-amber-50 row-span-9 row-start-7">3</div>
-            <div class=" row-span-4 bg-amber-200 border-2 border-amber-50 col-span-1 row-start-16">4</div>
+            <div class=" col-span-1  border-2 border-amber-50 row-span-9 row-start-7
+            grid grid-cols-2 grid-rows-8">
+              <div className="border-2 col-span-2 row-span-2 grid grid-cols-1 grid-rows-4"><div className='col-span-1 row-span-1 text-2xl font-medium text-amber-50'>Data Clustering</div><div className='col-span-1 row-span-3 text-[15px] font-medium text-[#F5C0C0]'>Unleash the power of data clustering—an unsupervised machine learning technique that uncovers patterns and groups similar data together without the need for labeled data.</div></div>
+              
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              
+
+            </div>
+            <div class=" row-span-4  border-2 border-amber-50 col-span-1 row-start-16  *:
+
+
+
+
+               grid grid-cols-3 grid-rows-4">
+              <div className="col-span-3 row-span-2 border-3 border-black grid grid-cols-1 grid-rows-4"><div className='col-span-1 row-span-1 text-2xl font-medium text-amber-50'>Data Clustering</div><div className='col-span-1 row-span-3 text-[15px] font-medium text-[#F5C0C0]'>Unleash the power of data clustering—an unsupervised machine learning technique that uncovers patterns and groups similar data together without the need for labeled data.</div></div>
+              
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://cdn.vectorstock.com/i/500p/43/83/rainbow-dots-vector-94383.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://cdn5.vectorstock.com/i/1000x1000/38/39/geometric-abstract-pattern-vector-21683839.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://media.istockphoto.com/id/2029197402/vector/colorful-half-tone-background.webp?b=1&s=612x612&w=0&k=20&c=rJUunnAplTC2gdzjb4cIqiU9xMXfEBxcmSNAH_y3alY=" className='bg-cover w-full h-full' ></img></div>
+           
+            </div>
             <div class=" col-span-1  border-r-2 border-l-2 border-amber-50 row-span-5 border-b-2 row-start-20 border-dashed"></div>
             {/* <div class=" col-span-1   border-l-2 border-r-2 border-amber-50 row-span-5 row-start-1"></div> */}
             <div class=" col-span-2   border-l-2 border-r-2 border-amber-50 row-span-5 row-start-1 text-wrap border-dashed text-white"><div class="text-7xl font-semibold text-center text-transparent">Build anything <br />with data</div> <div class="font-thin leading-5 pt-8 text-center text-transparent"> Quiuga, perferendis accusamus quo nostrum aut laborum dolor</div></div>
-            <div class=" col-span-1 bg-amber-600 border-2 border-amber-50 row-span-8 row-start-6">5</div>
-            <div class=" row-span-8 bg-amber-300 border-2 border-amber-50 col-span-1 row-start-14">6</div>
+            <div class=" col-span-1  border-2 border-amber-50 row-span-8 row-start-6
+             grid grid-cols-2 grid-rows-8">
+              <div className="border-2 col-span-2 row-span-2 grid grid-cols-1 grid-rows-4"><div className='col-span-1 row-span-1 text-2xl font-medium text-amber-50'>Data Clustering</div><div className='col-span-1 row-span-3 text-[15px] font-medium text-[#F5C0C0]'>Unleash the power of data clustering—an unsupervised machine learning technique that uncovers patterns and groups similar data together without the need for labeled data.</div></div>
+              
+               <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+           
+
+            </div>
+            <div class=" row-span-8  border-2 border-amber-50 col-span-1 row-start-14
+             grid grid-cols-2 grid-rows-8">
+              <div className="border-2 col-span-2 row-span-2 grid grid-cols-1 grid-rows-4"><div className='col-span-1 row-span-1 text-2xl font-medium text-amber-50'>Data Clustering</div><div className='col-span-1 row-span-3 text-[15px] font-medium text-[#F5C0C0]'>Unleash the power of data clustering—an unsupervised machine learning technique that uncovers patterns and groups similar data together without the need for labeled data.</div></div>
+              
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+           
+            </div>
             <div class=" col-span-1  border-r-2 border-l-2 border-amber-50 row-span-3 border-b-2 row-start-22 border-dashed"></div>
             <div class=" col-span-1   border-l-2 border-r-2 border-amber-50 row-span-3 row-start-1 border-dashed"></div>
-            <div class=" col-span-1 bg-amber-600 border-2 border-amber-50 row-span-8 row-start-4">7</div>
-            <div class=" row-span-4 bg-amber-100 border-2 border-amber-50 col-span-1 row-start-12">2</div>
+            <div class=" col-span-1  border-2 border-amber-50 row-span-8 row-start-4
+             grid grid-cols-2 grid-rows-8">
+              <div className="border-2 col-span-2 row-span-2 grid grid-cols-1 grid-rows-4"><div className='col-span-1 row-span-1 text-2xl font-medium text-amber-50'>Data Clustering</div><div className='col-span-1 row-span-3 text-[15px] font-medium text-[#F5C0C0]'>Unleash the power of data clustering—an unsupervised machine learning technique that uncovers patterns and groups similar data together without the need for labeled data.</div></div>
+              
+                 <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className="border-2 col-span-1 row-span-2 m-2 rounded-lg hover:scale-105 duration-300 overflow-hidden"><img src="https://img.freepik.com/premium-photo/sleek-tech-aesthetics-subtle-details-black-hightech-background_976492-481.jpg" className='bg-cover w-full h-full'></img></div>
+           
+            </div>
+            <div class=" row-span-4 border-2 border-amber-50 col-span-1 row-start-12
+           grid grid-cols-3 grid-rows-4">
+              <div className="col-span-3 row-span-2 border-3 border-black grid grid-cols-1 grid-rows-4"><div className='col-span-1 row-span-1 text-2xl font-medium text-amber-50'>Data Clustering</div><div className='col-span-1 row-span-3 text-[15px] font-medium text-[#F5C0C0]'>Unleash the power of data clustering—an unsupervised machine learning technique that uncovers patterns and groups similar data together without the need for labeled data.</div></div>
+              
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://cdn.vectorstock.com/i/500p/43/83/rainbow-dots-vector-94383.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://cdn5.vectorstock.com/i/1000x1000/38/39/geometric-abstract-pattern-vector-21683839.jpg" className='bg-cover w-full h-full'></img></div>
+              <div className='col-span-1 row-span-2 border-3 border-black m-1 rounded-lg hover:scale-105  duration-300 overflow-hidden'><img src="https://media.istockphoto.com/id/2029197402/vector/colorful-half-tone-background.webp?b=1&s=612x612&w=0&k=20&c=rJUunnAplTC2gdzjb4cIqiU9xMXfEBxcmSNAH_y3alY=" className='bg-cover w-full h-full' ></img></div>
+            </div>
             <div class=" col-span-1  border-r-2 border-l-2 border-b-2 border-amber-50 row-span-9 row-start-16 border-dashed"></div>
 
 
