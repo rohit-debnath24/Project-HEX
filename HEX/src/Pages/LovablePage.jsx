858d8956aa915632681cc02d279d4c0b31ea9179
rohit-debnath24 <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-07-21 08:07:00 +0530

    made lovable fully responsive

--- HEX/src/Pages/LovablePage.jsx
+++ HEX/src/Pages/LovablePage.jsx
@@ -4,19 +4,19 @@ const LovablePage = () => {
         <> <div className=''>
                 {/* */}
             <div className="grid w-60 h-160 m-auto grid-rows-6 grid-cols-4 border-l-1  sm:hidden "> 
-                <div className='col-span-1 h-15 mt-12 row-span-1 col-start-1 border border-amber-100'>1</div>
-                <div className='col-span-1 h-15 mt-12 row-span-1 col-start-2 border border-amber-100'>1</div>
-                <div className='col-span-1 h-15 mt-12 row-span-1 col-start-3 border border-amber-100'>1</div>
-                <div className='col-span-1 h-15 mt-12 row-span-1 col-start-4 border border-amber-100'>1</div>
+                <div className='col-span-1 h-15 mt-12 row-span-1 col-start-1 border border-amber-100'></div>
+                <div className='col-span-1 h-15 mt-12 row-span-1 col-start-2 border border-amber-100'></div>
+                <div className='col-span-1 h-15 mt-12 row-span-1 col-start-3 border border-amber-100'></div>
+                <div className='col-span-1 h-15 mt-12 row-span-1 col-start-4 border border-amber-100'></div>
                
-                {/* <div className=h-15 'col-span-1 row-span-1 border border-amber-100'>1</div> */}
+                {/* <div className=h-15 'col-span-1 row-span-1 border border-amber-100'></div> */}
          
                 <div className='col-span-4 row-span-4 row-start-2 border border-l-amber-100 border-r-amber-100  '> <div className=" text-amber-50 text-5xl text-center font-medium pt-20 pl-4 pr-4 border-t-1 border-amber-50  ">Loved by the best data teams</div><div className="flex justify-center text-amber-700 text-center mt-10 pl-3 pr-3 ">Hex helps companies of all sizes do more with their data.</div></div>
                 
-                <div className='col-span-1 h-15 row-span-1 col-start-1 border border-amber-100'>1</div>
-                <div className='col-span-1 h-15 row-span-1 col-start-2 border border-amber-100'>1</div>
-                <div className='col-span-1 h-15 row-span-1 col-start-3 border border-amber-100'>1</div>
-                <div className='col-span-1 h-15 row-span-1 col-start-4 border border-amber-100'>1</div>
+                <div className='col-span-1 h-15 row-span-1 col-start-1 border border-amber-100'></div>
+                <div className='col-span-1 h-15 row-span-1 col-start-2 border border-amber-100'></div>
+                <div className='col-span-1 h-15 row-span-1 col-start-3 border border-amber-100'></div>
+                <div className='col-span-1 h-15 row-span-1 col-start-4 border border-amber-100'></div>
                
                
             </div>
@@ -26,26 +26,26 @@ const LovablePage = () => {
 
 
             <div className="sm:grid sm:grid-border-2 h-[300px] lg:h-[600px] text-white w-200 lg:w-auto box-border border-[#827D77]/30 hidden sm-grid-cols-5 sm:grid-rows-5 gap-0  lg:pl-50 lg:pr-50 bg-whit md:ml-25 lg:ml-0 lg:pt-50 lg:mt-20 lg:mb-0 md:mt-35 ">
-                <div className="md:border-1 sm:grid sm:grid-cols-2  border-[#827D77]/30 "><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
-                <div className="md:border-1 sm:grid sm:grid-rows-2 border-[#827D77]/30  sm:col-span-3 sm:row-span-3 sm:colstart-2 sm:row-start-2 font-bold hidden "> <div className="sm:flex justify-center text-3xl md:text-6xl text-center md:m-6 hidden  text-amber-50 ">Loved by the <br />best data teams</div><div className="flex justify-center text-amber-700 mt-8">Hex helps companies of all sizes do more with their data.</div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2  border-[#827D77]/30 "><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
+                <div className="md:border-1 sm:grid sm:grid-rows-2 border-[#827D77]/30  sm:col-span-3 sm:row-span-3 sm:colstart-2 sm:row-start-2 font-bold hidden "> <div className="sm:flex justify-center text-3xl md:text-6xl text-center md:m-6 hidden  text-amber-50 ">Loved by the <br />best data teams</div><div className="flex justify-center text-amber-700 mt-16 lg:mt-8">Hex helps companies of all sizes do more with their data.</div></div>
                 <div className="md:border-1 sm:grid sm:grid-cols-2  border-[#827D77]/30  sm:col-start-2 sm:row-start-1"><div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div> <div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div></div>
                 <div className="md:border-1 sm:grid sm:grid-cols-2  border-[#827D77]/30  sm:col-start-3 sm:row-start-1"><div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div> <div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div></div>
                 <div className="md:border-1 sm:grid sm:grid-cols-2  border-[#827D77]/30  sm:col-start-4 sm:row-start-1"><div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div> <div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2  border-[#827D77]/30  sm:col-start-5 sm:row-start-1"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-1 sm:row-start-2"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-1 sm:row-start-3"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-1 sm:row-start-4"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-1 sm:row-start-5"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2  border-[#827D77]/30  sm:col-start-5 sm:row-start-1"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-1 sm:row-start-2"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-1 sm:row-start-3"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-1 sm:row-start-4"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-1 sm:row-start-5"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
                 <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-2 sm:row-start-5"><div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div> <div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div></div>
                 <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-3 sm:row-start-5"><div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div> <div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div></div>
                 <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-4 sm:row-start-5"><div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div> <div className="md:border-1 border-[#827D77]/30   sm:grid sm:grid-cols-2 "><div className='border-r-1 border-[#827D77]/30 '></div><div></div></div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-5 sm:row-start-5"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-5 sm:row-start-4"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-5 sm:row-start-3"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
-                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-5 sm:row-start-2"><div className="md:border-1 border-[#827D77]/30   ">1</div> <div className="md:border-1 border-[#827D77]/30   " >2</div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-5 sm:row-start-5"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-5 sm:row-start-4"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-5 sm:row-start-3"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
+                <div className="md:border-1 sm:grid sm:grid-cols-2 border-[#827D77]/30  sm:col-start-5 sm:row-start-2"><div className="md:border-1 border-[#827D77]/30   "></div> <div className="md:border-1 border-[#827D77]/30   " ></div></div>
             </div>
 
-            <div className="sm:grid sm:grid-border-2 h-280 md:max-h-90 sm:m-0 justify-around  grid-cols-1 grid-row-4 sm:grid-rows-1 sm:grid-cols-4  sm:gap-0 text-amber-50 lg:pl-50 lg:pr-50 md:ml-25 md:mr-24">
+            <div className="sm:grid sm:grid-border-2 h-280 md:max-h-90 sm:m-0 justify-around  grid-cols-1 grid-row-4 sm:grid-rows-1 sm:grid-cols-4  lg:ml-0 lg:mr-0  sm:gap-0 text-amber-50 lg:pl-50 lg:pr-50 md:ml-25 md:mr-21">
                 {/* <div className="border  border-[#827D77]/30  row-start-1 sm:row-start-1 justify-start w-60 h-65 sm:h-full sm:w-full"><img src="" className='m-auto md:m-0 md:relative top-12 h-5 md:h-10 left-5'></img><div className='pt-5 text-[22px] sm:text-2xl pl-5  h-20 sm:h-25'>Lorem ipsum dolor sit </div><div className='pl-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint a veritatis laudantium sed quasi quis!</div></div> */}
                 <div className="border bg-blue-500/20 shadow-blue-800/60 shadow-2xl hover:scale-95 duration-300 border-[#827D77]/30  row-start-1 m-auto mt-4 md:m-0 sm:row-start-1 grid justify-start w-60 h-65 sm:h-full sm:w-full"><img src="https://clipground.com/images/stubhub-png-1.png" className='m-auto mt-4 md:m-0 md:relative top-12 h-15 md:h-10 left-5'></img><div className='pt-5 text-[22px] font-semibold sm:text-2xl pl-5  h-20 sm:h-25'>Lorem ipsum dolor  </div><div className='md:pl-5 pl-4 pr-4 mb-8 md:m-0 text-center text-[15px] md:text-[15px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.  veritatis laudantium sed </div></div>
                 <div className="border  bg-blue-500/20 shadow-blue-800/60 shadow-2xl hover:scale-95 duration-300 border-[#827D77]/30  row-start-2 m-auto mt-4 md:m-0 sm:row-start-1 grid justify-start w-60 h-65 sm:h-full sm:w-full"><img src="https://sparkwise.ca/wp-content/uploads/2020/07/logo-dark-small-transparent.png" className='m-auto mt-4 md:m-0 md:relative top-12 h-8 md:h-10 left-5'></img><div className='pt-5 text-[22px] font-semibold sm:text-2xl pl-5  h-20 sm:h-25'>Lorem ipsum dolor  </div><div className='pl-5 mb-8 md:m-0 text-[15px] md:text-[15px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint a veritatis laudantium sed quasi quis!</div></div>
@@ -60,7 +60,7 @@ const LovablePage = () => {
         </div>
 
 
-            <div className="hidden md:grid grid-border-2 h-80  box-border border-[#827D77]/30  grid-cols-1 gap-4 sm:grid-cols-6 text-amber-50 md:pl-50 md:pr-50  md:mt-20 md:mb-20 pb-0">
+            <div className="hidden md:grid grid-border-2 h-80  box-border border-[#827D77]/30  grid-cols-1  gap-4 sm:grid-cols-6 text-amber-50 lg:pl-50 lg:pr-50  md:ml-25 md:mr-21 md:mt-20 lg:mb-20 pb-0">
                 <div className="border border-[#827D77]/30 m-auto h-40 md:h-80 col-span-1 w-40 sm:w-full sm:col-span-2 grid grid-rows-4">
                 <div className='row-span-3 p-4'><img src="https://images.ctfassets.net/mmgv7yhaaeds/6YeFcloKHpe0I7Ax0ihCah/df75892072dc00db9f5865be3a493e33/G2_Badges_Q3_2023.webp" className='h-full w-full'></img></div>
                 <div className='row-span-1 text-3xl font-medium pl-4'>Users lover Hex</div>
@@ -68,7 +68,7 @@ const LovablePage = () => {
                 </div>
                 <div className="border border-[#827D77]/30 m-auto  col-span-1 h-40 md:h-80 w-40 sm:w-full sm:col-span-4 grid grid-rows-4 ">
                 <div className='row-span-1'><img src="https://images.ctfassets.net/mmgv7yhaaeds/45K7YZyXLDrOeeYIN7Vi3y/d20ebb5f53a1f1bf6e5a061d828f2b09/Notion.svg" className='pl-20'></img></div>
-                <div className='row-span-2 text-2xl font-medium pl-20 pr-30'>"Our vision for Notion’s data team is that anyone, regardless of technical proficiency, is comfortable using data to answer their own questions — and Hex enables that.”</div>
+                <div className='row-span-2 text-2xl lg:text-2xl md:text-xl font-medium pl-20 pr-30'>"Our vision for Notion’s data team is that anyone, regardless of technical proficiency, is comfortable using data to answer their own questions — and Hex enables that.”</div>
                 <div className='pl-20 flex items-center'><div className='h-8 w-8 mr-4 rounded-full border border-white overflow-hidden'><img src="https://images.ctfassets.net/mmgv7yhaaeds/5RiuLohiQMsh4b1JS12ZnY/5d04efeba4e1fbc5e491ac0735a89119/abhishek-modi-headshot.jpg"></img></div> Abhishek Modi  ·  Software Engineer at Notion</div>
                 </div>
 
